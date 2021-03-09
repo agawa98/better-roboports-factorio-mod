ad28d8b60929c3cd496de949b538f68b9c571646
agawa98 <grz-bog@wp.pl>
2021-03-09 20:06:58 +0100

    first upload
    just added the mod

---
+++ data.lua
@@ -0,0 +1,3 @@
+data.raw.roboport.roboport.energy_source.input_flow_limit = "3MW"
+data.raw.roboport.roboport.energy_source.buffer_capacity = "1000MJ"
+data.raw.roboport.roboport.charging_energy = "20MW"
