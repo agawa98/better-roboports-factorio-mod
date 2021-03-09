d1dedf36f41ccb39b8bd6e5a44bbb75ee6672c8d
agawa98 <grz-bog@wp.pl>
2021-03-09 21:22:14 +0100

    update

---
+++ betterroboports/data.lua
@@ -0,0 +1,7 @@
+data.raw.roboport.roboport.energy_source.input_flow_limit = "20MW"
+data.raw.roboport.roboport.energy_source.buffer_capacity = "800MJ"
+data.raw.roboport.roboport.charging_energy = "20MW"
+data.raw.roboport.roboport.charging_offsets =
+    {
+      {-2.5, -1.5}, {2.5, -1.5}, {2.5, 2.5}, {-2.5, 2.5}
+    }
--- data.lua
+++
@@ -1,3 +0,0 @@
-data.raw.roboport.roboport.energy_source.input_flow_limit = "3MW"
-data.raw.roboport.roboport.energy_source.buffer_capacity = "1000MJ"
-data.raw.roboport.roboport.charging_energy = "20MW"
